09de76a138e48e5d8fdcf8dbcd1b881045e8e605
Ismail Magomedov <ismail@variant.net>
2024-02-25 14:58:35 +0400

    More animations

--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -1,24 +1,28 @@
 import styles from "./styles.module.css";
 import "dialogue/dist/styles.css";
 
+const phrase =
+  "In the shadowy depths of the ancient forest, a lone wanderer discovered the hidden entrance to the forgotten dungeon. With a heart pounding in anticipation and a flicker of magic in their eyes, they stepped forward, ready to face the unknown challenges ahead. The air was thick with the scent of mystery and danger, a silent whisper of the epic tales that were about to unfold.";
+
 export default function Home() {
   return (
     <>
-      <div className={styles.bold}>
-        {"Some long sentence".split("").map((char, index) => {
+      <div className={styles.bold} style={{ width: '400px'}}>
+        {phrase.split(" ").map((char, index) => {
           return (
-            <span key={index}>
-              <span
-                style={{ animationDelay: `-${index * 0.1}s` }}
-                className="dg-shake-y"
-              >
+            <>
+              <span key={index}>
                 <span
-                  style={{ animationDelay: `-${index * 0.22}s` }}
+                  style={{ animationDelay: `${index * 0.05}s` }}
+                  className="dg-swing"
                 >
-                  {char !== " " ? char : <span>&nbsp;</span>}
+                  <span className="dg-fadeIn" style={{ animationDelay: `${index * 0.05}s` }}>
+                    {char !== " " ? char : <span>&nbsp;</span>}
+                  </span>
                 </span>
               </span>
-            </span>
+              <span>&nbsp;</span>
+            </>
           );
         })}
       </div>
